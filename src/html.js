27f912deec934f735801552dc45c9abdee7b14b8
ctoogood <calumtoogood@gmail.com>
2020-10-25 06:49:27 +0000

    Added Icons script

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
