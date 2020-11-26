14fa6837aaf5ce44d08ceb7999c58cef6b8b8b60
ctoogood <calumtoogood@gmail.com>
2020-11-26 07:09:01 +0000

    Edited UIKit source

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,15 @@
-import './src/styles/main.scss'
+import "./src/styles/main.scss"
 
-import React from "react";
-import { ProductContextProvider } from "./src/context/ProductContext";
-import { CartContextProvider } from "./src/context/CartContext";
+import React from "react"
+import { ProductContextProvider } from "./src/context/ProductContext"
+import { CartContextProvider } from "./src/context/CartContext"
+import UIkit from "uikit"
+import Icons from "uikit/dist/js/uikit-icons"
+
+UIkit.use(Icons)
 
 export const wrapRootElement = ({ element }) => (
   <ProductContextProvider>
     <CartContextProvider>{element}</CartContextProvider>
   </ProductContextProvider>
-);
+)
--- src/components/shop/productList.js
+++ src/components/shop/productList.js
@@ -1,8 +1,7 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 import Product from "./product"
 
-const ProductList = ({products}) => {
+const ProductList = ({ products }) => {
   return (
     <section className="rrh_grid">
       {products.map(product => (
